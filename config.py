5460cc5c2df13ad9b4b976f252448f26e251bddf
Dranuragmaurya <162044160+Dranuragmaurya@users.noreply.github.com>
2025-05-23 16:39:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,12 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    API_ID = int(getenv("API_ID"))
-    API_HASH = getenv("API_HASH")
-    BOT_TOKEN = getenv("BOT_TOKEN")
-    MONGO_URI = getenv("MONGO_URI")
+    API_ID = int(getenv("20088801"))
+    API_HASH = getenv("b761ce9bcc78b5ad5d1d677762e831d1")
+    BOT_TOKEN = getenv("7572322597:AAEHoFL2QId_fLpukLDLeXZYRweuQBq6Ins")
+    MONGO_URI = getenv("mongodb+srv://Japusahu:Japusahu@japusahu.qnu3j.mongodb.net/?retryWrites=true&w=majority&appName=JAPUSAHU")
+
+# API_ID=20088801
+# API_HASH=b761ce9bcc78b5ad5d1d677762e831d1
+# BOT_TOKEN=7572322597:AAEHoFL2QId_fLpukLDLeXZYRweuQBq6Ins
+# MONGO_URI=mongodb+srv://Japusahu:Japusahu@japusahu.qnu3j.mongodb.net/?retryWrites=true&w=majority&appName=JAPUSAHU
